90382de9a352d430785d6e679f21e44f36971ce7
Werner Keil <werner.keil@gmx.net>
2017-02-20 01:13:44 +0100

    6: Fixed BlackBerry OS spelling
    Task-Url: https://github.com/OpenDDRmobi/openddr-java/issues/issues/6

--- classifier/client/src/main/java/mobi/openddr/classifier/Classifier.java
+++ classifier/client/src/main/java/mobi/openddr/classifier/Classifier.java
@@ -34,184 +34,185 @@ import mobi.openddr.classifier.loader.LoaderFactory;
  * @version 1.2
  */
 public class Classifier {
-    private static final Logger LOG = Logger.getLogger(Classifier.class
-	    .getName());
-    private static final java.util.regex.Pattern TEXT_SPLIT_PATTERN = java.util.regex.Pattern
-	    .compile(" |-|_|/|\\\\|\\[|\\]|\\(|\\)|;");
+	private static final Logger LOG = Logger.getLogger(Classifier.class.getName());
+	private static final java.util.regex.Pattern TEXT_SPLIT_PATTERN = java.util.regex.Pattern
+			.compile(" |-|_|/|\\\\|\\[|\\]|\\(|\\)|;");
 
-    private static long initCount = 0;
+	private static long initCount = 0;
 
-    // indexes
-    private Map<String, DeviceType> devices;
-    private Map<String, List<DeviceType>> patterns;
+	// indexes
+	private Map<String, DeviceType> devices;
+	private Map<String, List<DeviceType>> patterns;
 
-    private final Device unknown;
+	private final Device unknown;
 
-    public Classifier() {
-	devices = null;
-	patterns = null;
-	Map<String, String> uAttributes = new HashMap<String, String>();
-	uAttributes.put("id", Device.UNKNOWN_ID);
-	uAttributes = Collections.unmodifiableMap(uAttributes);
-	unknown = new Device(Device.UNKNOWN_ID, uAttributes);
-    }
-
-    public void initDeviceData(LoaderOption option) throws IOException {
-	initDeviceData(option, null);
-    }
-
-    public synchronized void initDeviceData(LoaderOption option, String path)
-	    throws IOException {
-	devices = LoaderFactory.getLoader(option, path).load();
-	initCount++;
-
-	if (initCount % 1000 == 0) {
-	    LOG.log(Level.WARNING,
-		    "Possible device data over-initialization detected");
+	public Classifier() {
+		devices = null;
+		patterns = null;
+		Map<String, String> uAttributes = new HashMap<String, String>();
+		uAttributes.put("id", Device.UNKNOWN_ID);
+		uAttributes = Collections.unmodifiableMap(uAttributes);
+		unknown = new Device(Device.UNKNOWN_ID, uAttributes);
 	}
 
-	if (devices == null) {
-	    patterns = null;
-	    return;
+	public void initDeviceData(LoaderOption option) throws IOException {
+		initDeviceData(option, null);
 	}
-	createIndex();
-    }
 
-    private void createIndex() {
-	patterns = new HashMap<String, List<DeviceType>>(8000);
+	public synchronized void initDeviceData(LoaderOption option, String path) throws IOException {
+		devices = LoaderFactory.getLoader(option, path).load();
+		initCount++;
 
-	for (DeviceType device : devices.values()) {
-	    for (Pattern pattern : device.getPatternSet().getPatterns()) {
-		for (int i = 0; i < pattern.getPatternParts().size(); i++) {
-		    String part = pattern.getPatternParts().get(i);
+		if (initCount % 1000 == 0) {
+			LOG.log(Level.WARNING, "Possible device data over-initialization detected");
+		}
 
-		    // duplicate
-		    if (patterns.get(part) != null) {
-			if (i == (pattern.getPatternParts().size() - 1)
-				&& !patterns.get(part).contains(device)) {
-			    patterns.get(part).add(device);
+		if (devices == null) {
+			patterns = null;
+			return;
+		}
+		createIndex();
+	}
+
+	private void createIndex() {
+		patterns = new HashMap<String, List<DeviceType>>(8000);
+
+		for (DeviceType device : devices.values()) {
+			for (Pattern pattern : device.getPatternSet().getPatterns()) {
+				for (int i = 0; i < pattern.getPatternParts().size(); i++) {
+					String part = pattern.getPatternParts().get(i);
+
+					// duplicate
+					if (patterns.get(part) != null) {
+						if (i == (pattern.getPatternParts().size() - 1) && !patterns.get(part).contains(device)) {
+							patterns.get(part).add(device);
+						}
+					} else {
+						List<DeviceType> single = new ArrayList<DeviceType>();
+						single.add(device);
+						patterns.put(part, single);
+					}
+				}
 			}
-		    } else {
-			List<DeviceType> single = new ArrayList<DeviceType>();
-			single.add(device);
-			patterns.put(part, single);
-		    }
 		}
-	    }
-	}
-    }
-
-    public Map<String, String> classify(String text) {
-	if (devices == null) {
-	    throw new RuntimeException("Uninitialized device index");
 	}
 
-	if (text == null) {
-	    return null;
-	}
-
-	Set<String> hitPatterns = new HashSet<String>();
-	Set<DeviceType> hitDevices = new HashSet<DeviceType>();
-	DeviceType winner = null;
-	Pattern winnerPattern = null;
-	LOG.log(Level.FINE, "classify: ''{0}''", text);
-	List<String> parts = split(text);
-
-	// generate ngrams upto size 4
-	for (int i = 0; i < parts.size(); i++) {
-	    String pattern = "";
-	    for (int j = 0; j < 4 && (j + i) < parts.size(); j++) {
-		pattern += parts.get(i + j);
-		List<DeviceType> dlist = patterns.get(pattern);
-		if (dlist != null) {
-		    hitPatterns.add(pattern);
-		    hitDevices.addAll(dlist);
-		    for (DeviceType device : dlist) {
-			LOG.log(Level.FINER,
-				"Hit found: ''{0}'' => id: ''{1}'' {2}",
-				new Object[] { pattern, device.getId(),
-					device.getPatternSet() });
-		    }
+	public Map<String, String> classify(String text) {
+		if (devices == null) {
+			throw new RuntimeException("Uninitialized device index");
 		}
-	    }
+
+		if (text == null) {
+			return null;
+		}
+
+		Set<String> hitPatterns = new HashSet<String>();
+		Set<DeviceType> hitDevices = new HashSet<DeviceType>();
+		DeviceType winner = null;
+		Pattern winnerPattern = null;
+		LOG.log(Level.FINE, "classify: ''{0}''", text);
+		List<String> parts = split(text);
+
+		// generate ngrams upto size 4
+		for (int i = 0; i < parts.size(); i++) {
+			String pattern = "";
+			String currPart = "";
+			for (int j = 0; (j + i) < parts.size(); j++) { // j < 4 &&
+				currPart = parts.get(i + j);
+				List<DeviceType> dlist = patterns.get(currPart);
+				if (dlist != null) {
+					hitPatterns.add(currPart);
+					hitDevices.addAll(dlist);
+					for (DeviceType device : dlist) {
+						LOG.log(Level.FINER, "Hit found: ''{0}'' => id: ''{1}'' {2}",
+								new Object[] { currPart, device.getId(), device.getPatternSet() });
+					}
+				} else {
+					pattern += currPart;
+					dlist = patterns.get(pattern);
+					if (dlist != null) {
+						for (DeviceType device : dlist) {
+							LOG.log(Level.FINER, "Hit found: ''{0}'' => id: ''{1}'' {2}",
+									new Object[] { pattern, device.getId(), device.getPatternSet() });
+						}
+					}
+				}
+			}
+		}
+
+		// look for the strongest hit
+		for (DeviceType device : hitDevices) {
+			Pattern pattern = device.getPatternSet().isValid(hitPatterns);
+			if (pattern == null) {
+				continue;
+			}
+
+			LOG.log(Level.FINER, "Hit candidate: ''{0}'' => ({1},{2})",
+					new Object[] { device.getId(), pattern.getType(), pattern.getRank() });
+
+			if (winnerPattern == null || pattern.getRank() > winnerPattern.getRank()) {
+				winner = device;
+				winnerPattern = pattern;
+			}
+		}
+
+		if (winner != null) {
+			LOG.log(Level.FINE, "Result: {0}", winner);
+
+			UserAgent userAgent = UserAgent.of(text);
+			LOG.log(Level.FINE, "User Agent: {0}", userAgent);
+			// fixFromUserAgent(winner, userAgent);
+			return adjustFromUserAgent(winner, userAgent).getAttributes();
+		} else {
+			return null;
+		}
 	}
 
-	// look for the strongest hit
-	for (DeviceType device : hitDevices) {
-	    Pattern pattern = device.getPatternSet().isValid(hitPatterns);
-	    if (pattern == null) {
-		continue;
-	    }
-
-	    LOG.log(Level.FINER,
-		    "Hit candidate: ''{0}'' => ({1},{2})",
-		    new Object[] { device.getId(), pattern.getType(),
-			    pattern.getRank() });
-
-	    if (winnerPattern == null
-		    || pattern.getRank() > winnerPattern.getRank()) {
-		winner = device;
-		winnerPattern = pattern;
-	    }
+	private static List<String> split(String text) {
+		String[] parts = TEXT_SPLIT_PATTERN.split(text);
+		List<String> nonemptyParts = new ArrayList<String>();
+		for (String part : parts) {
+			String normalizedPart = Pattern.normalize(part);
+			if (normalizedPart != null && !normalizedPart.isEmpty())
+				nonemptyParts.add(normalizedPart);
+		}
+		return nonemptyParts;
 	}
 
-	if (winner != null) {
-	    LOG.log(Level.FINE, "Result: {0}", winner);
-
-	    UserAgent userAgent = UserAgent.of(text);
-	    LOG.log(Level.FINE, "User Agent: {0}", userAgent);
-	    // fixFromUserAgent(winner, userAgent);
-	    return adjustFromUserAgent(winner, userAgent).getAttributes();
-	} else {
-	    return null;
+	public Device classifyDevice(String text) {
+		Map<String, String> m = classify(text);
+		if (m == null) {
+			return unknown;
+		}
+		return new Device(m.get("id"), m);
 	}
-    }
 
-    private static List<String> split(String text) {
-	String[] parts = TEXT_SPLIT_PATTERN.split(text);
-	List<String> nonemptyParts = new ArrayList<String>();
-	for (String part : parts) {
-	    String normalizedPart = Pattern.normalize(part);
-	    if (normalizedPart != null && !normalizedPart.isEmpty())
-		nonemptyParts.add(normalizedPart);
+	public int getDeviceCount() {
+		if (devices == null) {
+			return -1;
+		}
+		return devices.size();
 	}
-	return nonemptyParts;
-    }
 
-    public Device classifyDevice(String text) {
-	Map<String, String> m = classify(text);
-	if (m == null) {
-	    return unknown;
+	public int getPatternCount() {
+		if (patterns == null) {
+			return -1;
+		}
+		return patterns.size();
 	}
-	return new Device(m.get("id"), m);
-    }
 
-    public int getDeviceCount() {
-	if (devices == null) {
-	    return -1;
+	public long getNodeCount() {
+		if (patterns == null) {
+			return -1;
+		}
+		long count = 0;
+		for (List<DeviceType> pDevices : patterns.values()) {
+			count += pDevices.size();
+		}
+		return count;
 	}
-	return devices.size();
-    }
 
-    public int getPatternCount() {
-	if (patterns == null) {
-	    return -1;
+	Map<String, DeviceType> getDevices() {
+		return devices;
 	}
-	return patterns.size();
-    }
-
-    public long getNodeCount() {
-	if (patterns == null) {
-	    return -1;
-	}
-	long count = 0;
-	for (List<DeviceType> pDevices : patterns.values()) {
-	    count += pDevices.size();
-	}
-	return count;
-    }
-
-    Map<String, DeviceType> getDevices() {
-	return devices;
-    }
 }
